cf8af1c1cd1a8686c61e826984e2beece7bfb0b0
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-05-16 05:19:31 +0530

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const product = ref('Frido Active Socks');
+const productDescription = ref('Dual-Layer Moisture Control & Anti-Odor Design');
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product}}</h1>
+        <p>{{ productDescription }}</p>
       </div>
     </div>
   </div>
