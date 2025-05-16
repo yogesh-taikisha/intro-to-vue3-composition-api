899570e52cde5467bf82aa7a98549dc31b1dc2ee
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-05-16 05:52:24 +0530

    attribute binding

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <script setup>
 import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg';
 
-const product = ref('Socks')
+const product = ref('Socks');
+const productImage = ref(socksGreenImage);
+const productUrl = ref('url');
 
 </script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <!-- image goes here -->
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
+  <div class="nav-bar">
+    <div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <!-- image goes here -->
+           <!-- ubound image -->
+           <!-- <img src="socksGreenImage" alt="" srcset="">  -->
+
+           <!-- bound image to non-reactive variable -->
+           <!-- <img v-bind:src="socksGreenImage" alt="" srcset="">  -->
+
+           <!-- bound image to reactive variable -->
+           <img v-bind:src="productImage" alt="" srcset=""> 
+            <a :href="productUrl"></a>
+        </div>
+        <div class="product-info">
+          <h1>{{ product }}</h1>
+        </div>
       </div>
     </div>
   </div>
