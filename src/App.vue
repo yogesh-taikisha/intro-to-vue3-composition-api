5bef9dc69611688ecc9476e556c66db1ea9ada87
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-05-16 07:03:57 +0530

    list rendering

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,19 @@ const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+
+const variants = ref([
+  {id: 2234, color: 'green'},
+  {id: 2235, color: 'blue'}
+])
+
+const sizes = ref([
+  {id: 'extraSmall', code: 'XS' },
+  {id: 'small', code: 'S' },
+  {id: 'medium', code: 'M' },
+  {id: 'large', code: 'L' },
+  {id: 'extraLarge', code: 'XL' }
+])
 </script>
   
 <template>
@@ -19,6 +32,15 @@ const details = ref(['50% cotton', '30% wool', '20% polyester'])
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+
+        <!-- <div v-for="size in sizes" :key="sizes.id">{{ size.code }}</div> -->
+        <button v-for="size in sizes" :key="sizes.id">{{ size.code }}</button>
       </div>
     </div>
   </div>
