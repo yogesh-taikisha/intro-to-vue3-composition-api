30b9c2914dfefbfed1cf9bd145ddae8e780dd3b8
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-05-21 12:27:09 +0530

    components and props along with code challenge and it's solutions

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+    details: {
+        // prop validation
+        type: Array,
+        required: true
+    }
+});
+
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+
+</template>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { ref, computed } from 'vue'
+// relative path syntax
+// import socksGreenImage from '../assets/images/socks_green.jpeg'
+// import socksBlueImage from '../assets/images/socks_blue.jpeg'
+
+// absolute path syntax
+import socksGreenImage from '@/assets/images/socks_green.jpeg'
+import socksBlueImage from '@/assets/images/socks_blue.jpeg'
+import ProductDetails from './ProductDetails.vue';
+
+// prop passing
+const props = defineProps({
+    premium: {
+        // prop validation
+        type: Boolean,
+        required: true
+    }
+});
+
+const product = ref('Socks');
+const brand = ref('Vue Mastery');
+
+const selectedVariant = ref(0);
+
+const details = ref(['50% cotton', '30% wool', '20% polyester']);
+
+const variants = ref([
+  { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
+  { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
+]);
+
+const title = computed(() => {
+  return brand.value + ' ' + product.value
+});
+
+const image = computed(() => {
+  return variants.value[selectedVariant.value].image
+});
+
+const inStock = computed(() => {
+  return variants.value[selectedVariant.value].quantity > 0
+});
+
+const updateVariant = (index) => {
+  selectedVariant.value = index
+}
+const addToCart = () => cart.value += 1
+
+const shipping = computed(() => {
+    if(props.premium){
+        return 'Free';
+    }
+    else {
+        return 2.99;
+    }
+});
+</script>
+
+<template>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image" />
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+
+        <ProductDetails :details="details"/>
+
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.id"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
+          :style="{ backgroundColor: variant.color }"
+        ></div>
+        <button
+                class="button" 
+                :class="{ disabledButton: !inStock }"
+                :disabled="!inStock"
+                v-on:click="addToCart"
+              >
+                Add to cart
+        </button>
+      </div>
+    </div>
+  </div>
+
+</template>
